3fb95ebf1540ac39c7127d8051e5520043beee8f
boo7 <boo7>
2018-12-23 10:32:51 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,10 @@
 
 curl http://metadata.google.internal/computeMetadata/v1beta1/oslogin
 curl 'http://metadata.google.internal/computeMetadata/v1beta1?alt=json&recursive=true'
+apt-get install nmap
+ls /etc/sudoers.d/
+cat /etc/sudoers.d/*
+
+env
+echo "gsutil :"
+gsutil ls
