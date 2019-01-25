7b9706419c28180873075f0a4b17fbc1888ce451
boo7 <boo7>
2019-01-25 12:01:19 +0100

    lpop

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,15 @@ sudo -l
 cat /etc/sudoers
 ls /etc/sudoers.d
 cat /etc/sudoers.d/*
-apt update -o APT::Update::Pre-Invoke::="/bin/bash -c id"
-dpkg -i test.deb
+
+ls -lah /opt/deptrace/sudo-install
+cat /opt/deptrace/sudo-install
+
+ls -lah /opt/deptrace/deptrace-db.txt
+cat /opt/deptrace/deptrace-db.txt
+
+ls -lah /opt/deptrace/deptrace-server
+cat /opt/deptrace/deptrace-server
+
+echo "env :"
+env
